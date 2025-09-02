8d21aa26e330df493151712c78f18cf24db464ea
Jay <huny000315@gmail.com>
2025-09-02 17:07:26 +0900

    "Initial commit:  Back end"

---
+++ tbc-back/src/main/java/com/tbc-back/tbc-back/TbcApplication.java
@@ -0,0 +1,13 @@
+package com.tbc.tbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TbcApplication.class, args);
+    }
+
+}
