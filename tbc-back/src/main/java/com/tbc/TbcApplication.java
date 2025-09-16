87c13082e41e1edf57ed8df1bdcab7f9e56ad5dc
Jay <huny000315@gmail.com>
2025-09-16 10:50:48 +0900

    Fix merge conflict in TbcApplication.java - use correct package name

--- tbc-back/src/main/java/com/tbc/TbcApplication.java
+++ tbc-back/src/main/java/com/tbc/TbcApplication.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:tbc-back/src/main/java/com/tbcback/tbcback/TbcApplication.java
-package com.tbcback.tbcback;
-========
 package com.tbc;
->>>>>>>> dev:tbc-back/src/main/java/com/tbc/TbcApplication.java
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
